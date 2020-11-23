a613c4380abf25a8ae264f801499f52fe747f1dc
Arturo <gregory09ruiz@gmail.com>
2020-11-23 00:30:18 -0400

    import native storage in auth service

--- App/src/app/services/auth.service.ts
+++ App/src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
 import { tap } from 'rxjs/operators';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
 
 
 @Injectable({
