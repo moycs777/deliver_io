b07ae2e61b8b541a58261c916c2fd60ad96e559b
Arturo <gregory09ruiz@gmail.com>
2020-11-22 23:21:25 -0400

    register page generate

--- App/src/app/app-routing.module.ts
+++ App/src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./pages/auth/login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/auth/register/register.module').then( m => m.RegisterPageModule)
+  },
 ];
 
 @NgModule({
