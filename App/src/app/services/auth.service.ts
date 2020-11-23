5f75756fa8b8f6944d116ad621fcec35b659748b
Arturo <gregory09ruiz@gmail.com>
2020-11-23 00:40:48 -0400

    auth service ready now

--- App/src/app/services/auth.service.ts
+++ App/src/app/services/auth.service.ts
@@ -13,6 +13,84 @@ import { User } from '../models/user';
   providedIn: 'root'
 })
 export class AuthService {
+    isLoggedIn = false;
 
-  constructor() { }
+    token:any;
+
+    constructor(
+      private http: HttpClient,
+      private storage: NativeStorage,
+      private env: EnvService,
+    ) { }
+
+    login(email: String, password: String) {
+
+      return this.http.post(this.env.API_URL + 'auth/login',
+      {email: email, password: password,  unhashed_password: password, login_type: 'email'}
+      ).pipe(
+        tap(token => {
+          this.storage.setItem('token', token)
+          .then(
+            () => {
+              console.log('Token Stored');
+            },
+            error => console.error('Error storing item', error)
+          );
+          this.token = token;
+          this.isLoggedIn = true;
+          return token;
+        }),
+      );
+
+    }
+
+    register(name: String, email: String, password: String) {
+      return this.http.post(this.env.API_URL + 'auth/signup',
+        {name: name, email: email, password: password, login_type: 'email'}
+      )
+    }
+
+    logout() {
+      const headers = new HttpHeaders({
+        'Authorization': this.token["token_type"]+" "+this.token["access_token"]
+      });
+      return this.http.get(this.env.API_URL + 'auth/logout', { headers: headers })
+      .pipe(
+        tap(data => {
+          this.storage.remove("token");
+          this.isLoggedIn = false;
+          delete this.token;
+          return data;
+        })
+      )
+    }
+
+    // user() {
+    //   const headers = new HttpHeaders({
+    //     'Authorization': this.token["token_type"]+" "+this.token["access_token"]
+    //   });
+    //   return this.http.get<User>(this.env.API_URL + 'auth/user', { headers: headers })
+    //   .pipe(
+    //     tap(user => {
+    //       return user;
+    //     })
+    //   )
+    // }
+    getToken() {
+      return this.storage.getItem('token').then(
+        data => {
+          this.token = data;
+          if(this.token != null) {
+            this.isLoggedIn=true;
+          } else {
+            this.isLoggedIn=false;
+          }
+        },
+        error => {
+          this.token = null;
+          this.isLoggedIn=false;
+        }
+      );
+    }
+    
 }
