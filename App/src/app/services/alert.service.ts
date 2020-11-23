1bf4ea7879ed71fc1cff60b8cc2405f54dc5caf5
Arturo <gregory09ruiz@gmail.com>
2020-11-23 00:29:01 -0400

    formart documents

--- App/src/app/services/alert.service.ts
+++ App/src/app/services/alert.service.ts
@@ -7,14 +7,14 @@ import { ToastController } from '@ionic/angular';
 export class AlertService {
 
   constructor(private toastController: ToastController) { }
-  async presentToast(message: any) {
-    const toast = await this.toastController.create({
-      message: message,
-      duration: 2000,
-      animated:true,
-      position: 'top',
-      color: 'dark'
-    });
-    toast.present();
-  }
+    async presentToast(message: any) {
+      const toast = await this.toastController.create({
+        message: message,
+        duration: 2000,
+        animated:true,
+        position: 'top',
+        color: 'dark'
+      });
+      toast.present();
+    }
 }
