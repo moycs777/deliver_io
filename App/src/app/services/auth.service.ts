a594da21c80a886fc5b6884504689ea82e610580
Arturo <gregory09ruiz@gmail.com>
2020-11-23 00:29:59 -0400

    import tap in auth service

--- App/src/app/services/auth.service.ts
+++ App/src/app/services/auth.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
